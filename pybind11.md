ed8e3ded28a8e6719869c3c304c4b22caa64c8c4
xjkdev <xjk2008@gmail.com>
2022-01-04 16:51:26 +0800

    Initial Commit

---
+++ pybind11.md
@@ -0,0 +1,126 @@
+# 使用Pybind11
+
+## 编译系统
+### CMake
+```
+add_subdirectory(pybind11)
+pybind11_add_module(cmake_example src/main.cpp)
+```
+
+### Setuptools
+```python
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
+
+ext_modules = [
+    Pybind11Extension("python_example",
+        ["src/main.cpp"],
+        # Example: passing in the version to the compiled code
+        define_macros = [('VERSION_INFO', __version__)],
+        ),
+]
+
+setup(
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    python_requires=">=3.6",
+)
+```
+
+## Numpy
+
+Methods of `py::array_t`:
+
+
+- `.ndim()` returns the number of dimensions
+- `.data(1, 2, ...)` and `r.mutable_data(1, 2, ...)` returns a pointer to the const T or T data, respectively, at the given indices. The latter is only available to proxies obtained via a.mutable_unchecked().
+- `.itemsize()` returns the size of an item in bytes, i.e. sizeof(T).
+- `.ndim()` returns the number of dimensions.
+- `.shape(n)` returns the size of dimension n
+- `.size()` returns the total number of elements (i.e. the product of the shapes).
+- `.nbytes()` returns the number of bytes used by the referenced elements (i.e. itemsize() times size()).
+
+- `.request()` returns `buffer_info` of array
+- `.unchecked<n>()` and `.mutable_unchecked<n>()` returns direct access proxy of dimension n. Use `Proxy(i, j, k, ...)` to access data[i,j,k,...].
+
+Example codes:
+```cpp
+
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+
+namespace py = pybind11;
+
+// Use pointer from buffer_info.
+
+py::array_t<double> add_arrays(py::array_t<double> input1, py::array_t<double> input2) {
+    py::buffer_info buf1 = input1.request(), buf2 = input2.request();
+
+    if (buf1.ndim != 1 || buf2.ndim != 1)
+        throw std::runtime_error("Number of dimensions must be one");
+
+    if (buf1.size != buf2.size)
+        throw std::runtime_error("Input shapes must match");
+
+    /* No pointer is passed, so NumPy will allocate the buffer */
+    auto result = py::array_t<double>(buf1.size);
+
+    py::buffer_info buf3 = result.request();
+
+
+    double *ptr1 = static_cast<double *>(buf1.ptr);
+    double *ptr2 = static_cast<double *>(buf2.ptr);
+    double *ptr3 = static_cast<double *>(buf3.ptr);
+
+    for (size_t idx = 0; idx < buf1.shape[0]; idx++)
+        ptr3[idx] = ptr1[idx] + ptr2[idx];
+
+    return result;
+}
+
+// Direct Access
+
+m.def("sum_3d", [](py::array_t<double> x) {
+    auto r = x.unchecked<3>(); // x must have ndim = 3; can be non-writeable
+    double sum = 0;
+    for (py::ssize_t i = 0; i < r.shape(0); i++)
+        for (py::ssize_t j = 0; j < r.shape(1); j++)
+            for (py::ssize_t k = 0; k < r.shape(2); k++)
+                sum += r(i, j, k);
+    return sum;
+});
+m.def("increment_3d", [](py::array_t<double> x) {
+    auto r = x.mutable_unchecked<3>(); // Will throw if ndim != 3 or flags.writeable is false
+    for (py::ssize_t i = 0; i < r.shape(0); i++)
+        for (py::ssize_t j = 0; j < r.shape(1); j++)
+            for (py::ssize_t k = 0; k < r.shape(2); k++)
+                r(i, j, k) += 1.0;
+}, py::arg().noconvert());
+```
+
+buffer_info Definination:
+```cpp
+struct buffer_info {
+    void *ptr;
+    py::ssize_t itemsize;
+    std::string format;
+    py::ssize_t ndim;
+    std::vector<py::ssize_t> shape;
+    std::vector<py::ssize_t> strides;
+};
+```
+
+## 已知问题
+### CMake PCL造成未定义符号`__Py_ZeroStruct`
+原因：CMake中使用`find_package(PCL)`会引入vtk的python2.7链接路径，造成问题。
+
+解决方法：不要使用`find_package(PCL)`来引入PCL。用以下代替：
+```CMake
+find_package(Eigen3 REQUIRED) # Eigen3 is required
+include_directories(${EIGEN3_INCLUDE_DIRECTORIES})
+
+include_directories(/usr/include/pcl-1.8)
+link_directories(/usr/lib/x86_64-linux-gnu)
+pybindll_add_module(example example.cpp)
+target_link_libraries(example PUBLIC pcl_io) # PCL Libraries
+```
---
+++ snippet/CMakeExtension.setup.py
@@ -0,0 +1,181 @@
+import os
+import re
+import sys
+import pathlib
+import subprocess
+from typing import Dict, List
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import pybind11
+
+# Convert distutils Windows platform specifiers to CMake -A arguments
+PLAT_TO_CMAKE = {
+    "win32": "Win32",
+    "win-amd64": "x64",
+    "win-arm32": "ARM",
+    "win-arm64": "ARM64",
+}
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name: str, cmake_args: Dict[str, str] = None, cmakelists_dir: str = None, target_name=None):
+        """
+            name: name of extension, package format.
+                  for example: 'example_dir.example' will output a example extension in example_dir folder.
+            cmake_args: dict of cmake arguments, equalivant to cmake command arguments '-DKey=Value'.
+                  for example, assigning pybind11 path to cmake:
+                      `cmake_args={'pybind11_dir': Pybind11.get_cmake_path()}`
+            cmakelists_dir: parent path to CMakeLists.txt, default to package dir.
+                  for example: for name='example.ext', it will default use ./example/CMakeLists.txt.
+        """
+        # don't invoke the original build_ext for this special extension
+        super().__init__(name, sources=[])
+        self.cmake_args = cmake_args or {}
+        package_parts = name.split('.')
+        if cmakelists_dir is None:
+            self.cmakelists_dir = os.path.join('.', *package_parts[:-1])
+        else:
+            self.cmakelists_dir = cmakelists_dir
+
+        if target_name is None:
+            if cmakelists_dir is None:
+                self.target_name = package_parts[-1]
+            else:
+                self.target_name = None
+        else:
+            self.target_name = target_name
+
+
+def CMakeExtensionize(names: List[str], cmake_args: Dict[str, str] = None, cmakelists_dir: str = None) -> List[CMakeExtension]:
+    """
+        Accept multiple extension in one CMakeLists
+    """
+    return [CMakeExtension(name, cmake_args, cmakelists_dir) for name in names]
+
+
+class CMakeBuild(build_ext):
+    def build_extension(self, ext):
+        if not isinstance(ext, CMakeExtension):
+            return super().build_extension(ext)
+        cmakelists_dir = pathlib.Path(ext.cmakelists_dir).absolute()
+        temp_subdir = "CMakeTemp_" + "".join(x if x.isalnum() else '_' for x in ext.cmakelists_dir)
+        # these dirs will be created in build_py, so if you don't have
+        # any python sources to bundle, the dirs will be missing
+        build_temp = pathlib.Path(self.build_temp) / temp_subdir
+        build_temp.mkdir(parents=True, exist_ok=True)
+        extdir = pathlib.Path(self.get_ext_fullpath(ext.name)).parent
+        extdir.mkdir(parents=True, exist_ok=True)
+
+        debug = int(os.environ.get("DEBUG", 0)) if self.debug is None else self.debug
+        cfg = 'Debug' if debug else 'Release'
+
+        # CMake lets you override the generator - we need to check this.
+        # Can be set with Conda-Build, for example.
+        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+
+        cmake_args = [
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}'.format(extdir.absolute()),
+            "-DPYTHON_EXECUTABLE={}".format(sys.executable),
+            '-DCMAKE_BUILD_TYPE={}'.format(cfg),
+            # INFO: You can add custom cmake_args here.
+        ]
+
+        build_args = []
+        # Adding CMake arguments set as environment variable
+        # (needed e.g. to build for ARM OSx on conda-forge)
+        if "CMAKE_ARGS" in os.environ:
+            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
+        for k, v in ext.cmake_args.items():
+            cmake_args.append('-D{}={}'.format(k, v))
+
+        if self.compiler.compiler_type != "msvc":
+            # Using Ninja-build since it a) is available as a wheel and b)
+            # multithreads automatically. MSVC would require all variables be
+            # exported for Ninja to pick it up, which is a little tricky to do.
+            # Users can override the generator with CMAKE_GENERATOR in CMake
+            # 3.15+.
+            if not cmake_generator:
+                try:
+                    import ninja  # noqa: F401
+                    cmake_args += ["-GNinja"]
+                except ImportError:
+                    pass
+
+        else:
+            # Single config generators are handled "normally"
+            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
+
+            # CMake allows an arch-in-generator style for backward compatibility
+            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
+
+            # Specify the arch if using MSVC generator, but only if it doesn't
+            # contain a backward-compatibility arch spec already in the
+            # generator name.
+            if not single_config and not contains_arch:
+                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
+
+            # Multi-config generators have a different way to specify configs
+            if not single_config:
+                cmake_args += [
+                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
+                ]
+                build_args += ["--config", cfg]
+
+        if sys.platform.startswith("darwin"):
+            # Cross-compile support for macOS - respect ARCHFLAGS if set
+            archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
+            if archs:
+                cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
+
+        # example of build args
+        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
+        # across all generators.
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+            # self.parallel is a Python 3 only way to set parallel jobs by hand
+            # using -j in the build_ext call, not supported by pip or PyPA-build.
+            if hasattr(self, "parallel") and self.parallel:
+                # CMake 3.12+ only.
+                build_args += [f"-j{self.parallel}"]
+
+        if ext.target_name is not None:
+            build_args += ["--target={}".format(ext.target_name)]
+
+        subprocess.check_call(
+            ["cmake", str(cmakelists_dir)] + cmake_args, cwd=str(build_temp)
+        )
+
+        if not self.dry_run:
+            subprocess.check_call(
+                ["cmake", "--build", "."] + build_args, cwd=str(build_temp)
+            )
+
+
+__doc__ = """
+Example:
+Project Tree:
+├── ext1
+│   ├── CMakeLists.txt
+│   ├── ext1.cpython-39-darwin.so  # Extension output to here (--inplace).
+│   └── src
+│       └── ext1.cpp
+├── ext2
+│   ├── CMakeLists.txt
+│   ├── ext2.cpython-39-darwin.so  # Extension output to here (--inplace).
+│   ├── ext3.cpython-39-darwin.so  # Extension output to here (--inplace).
+│   └── src
+│       ├── ext2.cpp
+│       └── ext3.cpp
+└── setup.py
+
+setup(
+    name='example',
+    version='0.1',
+    packages=['example'],
+    ext_modules=[CMakeExtension('ext1.ext1', cmake_args={"pybind11_DIR": pybind11.get_cmake_dir()}),
+                 *CMakeExtensionize(['ext2.ext2', 'ext2.ext3'], cmake_args={"pybind11_DIR": pybind11.get_cmake_dir()})],
+    cmdclass={
+        'build_ext': CMakeBuild,
+    }
+)
+"""
